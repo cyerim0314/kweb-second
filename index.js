a01b488a307d91b197dfdb7e56c4b7fe6371b730
Yerim Cho <cyerim0314@naver.com>
2020-10-18 11:59:15 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const axios = require('axios');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+   try {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        axios.get('https://api.github.com/repos/nodejs/node')
+        .then((response) => {
+            const {stargazers_count, forks_count, open_issues_count} = response.data;
+            res.end(`Stargazers: ${stargazers_count}\nForks: ${forks_count}\nOpen Issues: ${open_issues_count}`);
+        })
+   } catch (e) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internet error issued.');
+   }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
